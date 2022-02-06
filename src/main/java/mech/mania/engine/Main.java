124ce8e0b778f591fb9e45884be78d4ce0f4e5fe
Rishi <rishi.sek@gmail.com>
2022-02-06 13:41:26 -0600

    Outline workflow in main

--- src/main/java/mech/mania/engine/Main.java
+++ src/main/java/mech/mania/engine/Main.java
@@ -1,4 +1,35 @@
 package mech.mania.engine;
 
 public class Main {
+  public void main(String[] args){
+    /**
+     * A single turn:
+     * - Use
+     * - Move
+     * - Attack
+     * - Buy
+     *
+     * DecisionSet becomes a compilation of these 4. At the end of a turn, decision_set is written to JSON.
+     *
+     * Turn {
+     * players: [
+     *  Player {
+     *    // state
+     *  }
+     * ],
+     * decision_sets: [
+     *  DecisionSet {
+     *    use, move, attack, buy
+     *  }
+     * ]
+     * }
+     */
+
+    /*
+    1. Create GameState
+    2. For each turn of game,
+        a. execute use, move, ...
+        b. render turn
+     */
+  }
 }
