2f9866b912b7b3185a404603bd42b0046871a041
Xavier Routh <xavierrouth@gmail.com>
2022-02-15 19:17:41 -0600

    Added constructor for position and added new constructor for MoveAction using position

--- src/main/java/mech/mania/engine/action/MoveAction.java
+++ src/main/java/mech/mania/engine/action/MoveAction.java
@@ -1,5 +1,7 @@
 package mech.mania.engine.action;
 
+import mech.mania.engine.player.Position;
+
 public class MoveAction extends Action{
     private int x_dest;
     private int y_dest;
@@ -9,7 +11,15 @@ public class MoveAction extends Action{
         this.x_dest = x_dest;
         this.y_dest = y_dest;
     }
+    public MoveAction(int executingPlayerIndex, Position destination) {
+        super(executingPlayerIndex);
+        this.x_dest = destination.getX();
+        this.y_dest = destination.getY();
+    }
 
+    public Position getPosition() {
+        return new Position(this.getX_dest(), this.getY_dest());
+    }
     public int getX_dest() {
         return x_dest;
     }
