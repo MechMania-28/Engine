3d679616b5f7af71439e0a64240a233fe116d9d0
Xavier Routh <xavierrouth@gmail.com>
2022-02-16 16:23:29 -0600

    Implement movementTest

--- src/test/java/mech/mania/engine/ActionTest.java
+++ src/test/java/mech/mania/engine/ActionTest.java
@@ -1,27 +1,42 @@
 package mech.mania.engine;
 import mech.mania.engine.GameState;
 import mech.mania.engine.action.MoveAction;
+import mech.mania.engine.player.CharacterClass;
 import mech.mania.engine.player.PlayerState;
 import mech.mania.engine.player.Position;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ActionTest {
 
-    private PlayerState playerState;
+    private PlayerState testPlayer;
     private GameState gameState;
 
     @BeforeEach
     public void setup() {
 
-        GameState gameState = new GameState();
         /*
-        create a new player state with some default configurations.
+        Configure a default gameState
          */
-        PlayerState playerState = new PlayerState();
 
+        List<PlayerState> playerStateList = Arrays.asList(new PlayerState[4]);
+
+        playerStateList.set(0, new PlayerState(CharacterClass.KNIGHT, new Position(0, 0)));
+        playerStateList.set(1, new PlayerState());
+        playerStateList.set(2, new PlayerState());
+        playerStateList.set(3, new PlayerState());
+
+        gameState = new GameState(playerStateList);
+
+        /*
+        Set the first player as our test player
+         */
+        testPlayer = gameState.getPlayerStateByIndex(0);
 
     }
 
@@ -40,23 +55,41 @@ public class ActionTest {
 
         int playerIndex = 0;
 
-        Position destination = new Position(2, 2);
+        /*
+        VALID: test in range move action
+         */
+        Position destination = new Position(2, 0);
         MoveAction moveAction = new MoveAction(playerIndex, destination);
         gameState.executeMove(moveAction);
 
-        // Implement creation of playerState and gameState to continue.
-
-        // Position actualPosition = gameState.playerStateList[playerIndex].getPosition();
+        Position actualPosition = testPlayer.getPosition();
 
         /*
         assert player's position is your desired position after the movement.
          */
-        // assertEquals(actualPosition, destination);
+        assertEquals(actualPosition, destination);
+
 
         /*
-
-
-
+        INVALID: test out of range move action
          */
+        destination = new Position(5, 0);
+        moveAction = new MoveAction(playerIndex, destination);
+        gameState.executeMove(moveAction);
+
+        actualPosition = testPlayer.getPosition();
+
+        assertNotEquals(actualPosition, destination);
+
+        /*
+        INVALID: test out of board range move action
+         */
+        destination = new Position(Config.BOARD_SIZE + 2, 0);
+        moveAction = new MoveAction(playerIndex, destination);
+        gameState.executeMove(moveAction);
+
+        actualPosition = testPlayer.getPosition();
+
+        assertNotEquals(actualPosition, destination);
     }
 }
