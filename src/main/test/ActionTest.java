e0ebd581219cad6f156778edb372a07540c5ed77
hanby <boyouhan@gmail.com>
2022-02-15 19:26:04 -0600

    Quickfix: Added a sample test so that other unit tests could be written easier.

---
+++ src/main/test/ActionTest.java
@@ -0,0 +1,27 @@
+import mech.mania.engine.GameState;
+import mech.mania.engine.player.PlayerState;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ActionTest {
+
+    private PlayerState playerState;
+
+    @Before
+    public void setup() {
+        /*
+        create a new player state with some default configurations.
+         */
+    }
+
+    @Test
+    public void testMovementAction() {
+        /*
+        create and excecute the moveaction on the player.
+         */
+
+        /*
+        assert player's position is your desired position after the movement.
+         */
+    }
+}
