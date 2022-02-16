206395c565b09cd2520f59bbdaf0e8036e707fb1
Xavier Routh <xavierrouth@gmail.com>
2022-02-16 14:10:09 -0600

    Changed utility to use config board size

--- src/main/java/mech/mania/engine/Utility.java
+++ src/main/java/mech/mania/engine/Utility.java
@@ -1,10 +1,13 @@
 package mech.mania.engine;
 
+import mech.mania.engine.Config;
+
 public class Utility {
     public static int manhattanDistance(int x0, int x1, int y0, int y1){
         return Math.abs(x1 - x0) + Math.abs(y1 - y0);
     }
     public static boolean inBounds(int x, int y){
-        return ((x >= 0) && ( x <= 9) && (y >= 0) && (y <= 9 ));
+        /** Assume board runs from 0 to BOARD_SIZE - 1 */
+        return ((x >= 0) && ( x < Config.BOARD_SIZE) && (y >= 0) && (y < Config.BOARD_SIZE));
     }
 }
