698ba0d837482a62d66299991ae818b39a7ee64a
Rishi <rishi.sek@gmail.com>
2022-02-22 11:44:48 -0600

    Implement engine-level execution

--- src/main/java/mech/mania/engine/GameEngine.java
+++ src/main/java/mech/mania/engine/GameEngine.java
@@ -3,6 +3,7 @@ package mech.mania.engine;
 import mech.mania.engine.action.*;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class GameEngine {
@@ -18,10 +19,33 @@ public class GameEngine {
 
   /**
    * Executes a given action as an action of the current phase state.
+   *
    * @param action Action to be executed.
    */
   public void execute(Action action) {
+    if (executed.get(action.getExecutingPlayerIndex())) return;
+    switch (phase) {
+      case USE:
+        state.executeUse((UseAction) action);
+        break;
+      case MOVE:
+        state.executeMove((MoveAction) action);
+        break;
+      case ATTACK:
+        state.executeAttack((AttackAction) action);
+        break;
+      case BUY:
+        state.executeBuy((BuyAction) action);
+        break;
+    }
+    executed.set(action.getExecutingPlayerIndex(), true);
 
+    // If all players have executed an Action for the current phase, reset and move to the next
+    // phase.
+    if (executed.stream().allMatch(Boolean::valueOf)) {
+      Collections.fill(executed, Boolean.FALSE);
+      this.phase = phase.next();
+    }
   }
 
   public void endPhase() {
