b765e317ca0829aea1cf8a2ede42fb3673fc1904
Rishi <rishi.sek@gmail.com>
2022-02-23 14:34:20 -0600

    Rename class

--- src/test/java/mech/mania/engine/MoveActionTest.java
+++ src/test/java/mech/mania/engine/MoveActionTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ActionTest {
+public class MoveActionTest {
 
     private PlayerState testPlayer;
     private GameState gameState;
