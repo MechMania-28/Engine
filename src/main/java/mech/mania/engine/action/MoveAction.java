10a697aa0ba7a25261296f94e4829667be02e147
Xavier Routh <xavierrouth@gmail.com>
2022-02-23 00:17:34 -0600

    made destination attribute of MoveAction final

--- src/main/java/mech/mania/engine/action/MoveAction.java
+++ src/main/java/mech/mania/engine/action/MoveAction.java
@@ -4,7 +4,7 @@ package mech.mania.engine.action;
 import mech.mania.engine.player.Position;
 
 public class MoveAction extends Action{
-    private Position destination;
+    private final Position destination;
 
     public MoveAction(int executingPlayerIndex, Position destination) {
         super(executingPlayerIndex);
