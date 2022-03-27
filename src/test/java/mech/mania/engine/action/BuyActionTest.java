1b93b12c01a7dd93e7b50ae5bb92690f0cb35590
Xavier Routh <xavierrouth@gmail.com>
2022-03-27 14:42:43 -0500

    BuyAction unit tests

--- src/test/java/mech/mania/engine/action/BuyActionTest.java
+++ src/test/java/mech/mania/engine/action/BuyActionTest.java
@@ -1,2 +1,114 @@
-package mech.mania.engine.action;public class BuyActionTest {
+package mech.mania.engine.action;
+
+import mech.mania.engine.*;
+import java.util.Arrays;
+import java.util.List;
+
+
+import mech.mania.engine.player.CharacterClass;
+import mech.mania.engine.player.Item;
+import mech.mania.engine.player.PlayerState;
+import mech.mania.engine.player.Position;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static mech.mania.engine.Config.BOARD_SIZE;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class BuyActionTest {
+
+    private PlayerState testP1;
+    private PlayerState testP2;
+    private GameState gameState;
+
+    @BeforeEach
+    public void setup() {
+        // Configure a default gameState
+
+        List<PlayerState> playerStateList = Arrays.asList(new PlayerState[4]);
+        playerStateList.set(0, new PlayerState(CharacterClass.KNIGHT, new Position(0, 0)));
+        playerStateList.set(1, new PlayerState(CharacterClass.ARCHER, new Position(BOARD_SIZE-1, 0)));
+        playerStateList.set(2, new PlayerState());
+        playerStateList.set(3, new PlayerState());
+
+        gameState = new GameState(playerStateList);
+
+        testP1 = gameState.getPlayerStateByIndex(0);
+        testP2 = gameState.getPlayerStateByIndex(1);
+        testP1.setItem(Item.NULL_ITEM);
+        testP2.setItem(Item.NULL_ITEM);
+    }
+
+    @Test
+    /* Test a player successfully buying an item  */
+    public void buyItem() {
+
+        Item anemoiWings = Item.ANEMOI_WINGS;
+
+        testP1.setGold(100);
+
+        // Create new BuyAction
+        BuyAction buyAnemoiWings = new BuyAction(0, anemoiWings);
+        gameState.executeBuy(buyAnemoiWings);
+
+        // Test that the player has successfully acquired the item
+        assertEquals(testP1.getItem(), Item.ANEMOI_WINGS);
+        // Test that the gold has decreased by the correct amount
+        assertEquals(testP1.getGold(), 100 - Item.ANEMOI_WINGS.getCost());
+
+    }
+
+    @Test
+    /* Test a player buying a new item */
+    public void replaceItem() {
+        buyItem();
+        BuyAction buyStrengthPotion  = new BuyAction(0, Item.STRENGTH_POTION);
+        gameState.executeBuy(buyStrengthPotion);
+
+        assertEquals(testP1.getItem(), Item.STRENGTH_POTION);
+        assertNotEquals(testP1.getItem(), Item.ANEMOI_WINGS);
+
+        assertEquals(testP1.getGold(), 100 - Item.ANEMOI_WINGS.getCost() - Item.STRENGTH_POTION.getCost());
+    }
+
+    @Test
+    /* Test two players buying the same item */
+    public void buySameItem() {
+        testP1.setGold(100);
+        testP2.setGold(100);
+
+        BuyAction player1Buy = new BuyAction(0, Item.ANEMOI_WINGS);
+        BuyAction player2Buy = new BuyAction(1, Item.ANEMOI_WINGS);
+
+        gameState.executeBuy(player1Buy);
+        gameState.executeBuy(player2Buy);
+
+        assertEquals(testP1.getItem(), Item.ANEMOI_WINGS);
+        assertEquals(testP2.getItem(), Item.ANEMOI_WINGS);
+
+    }
+
+    @Test
+    /* Test a player that can't afford an item */
+    public void cantAffordItem() {
+        testP1.setGold(Item.ANEMOI_WINGS.getCost()-1);
+
+        BuyAction buyAction = new BuyAction(0, Item.ANEMOI_WINGS);
+
+        assertNotEquals(testP1.getItem(), Item.ANEMOI_WINGS);
+        assertEquals(testP1.getGold(), Item.ANEMOI_WINGS.getCost()-1);
+    }
+
+    @Test
+    /* Test a player in the wrong spot (not in spawn) */
+    public void notInShop() {
+        testP1.getPosition().translate(new Position(1, 1));
+        testP1.setGold(100);
+
+        BuyAction buyAction = new BuyAction(0, Item.ANEMOI_WINGS);
+
+        assertNotEquals(testP1.getItem(), Item.ANEMOI_WINGS);
+        assertEquals(testP1.getGold(), 100);
+    }
 }
