da525726003857c1fab7f9a08fb7e297b9e7c528
Rishi <rishi.sek@gmail.com>
2022-04-01 16:54:31 -0500

    Add config for gold per turn

--- src/main/java/mech/mania/engine/Config.java
+++ src/main/java/mech/mania/engine/Config.java
@@ -4,6 +4,7 @@ public class Config {
   public static final int BOARD_SIZE = 10;
   public static final int MAX_SCORE = 100;
   public static final int TURNS = 50;
+  public static final int GOLD_PER_TURN = 10;
   /*
   etc.
    */
