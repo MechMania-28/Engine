72c6bc0d59255881e2337f4997865cef126fc247
Rishi <rishi.sek@gmail.com>
2022-04-01 17:02:11 -0500

    Add turn property

--- src/main/java/mech/mania/engine/GamePhase.java
+++ src/main/java/mech/mania/engine/GamePhase.java
@@ -8,17 +8,20 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GamePhase {
-  /** A list of states of the 4 players. */
-  @JsonProperty("player_states")
-  public List<PlayerState> playerStates = Arrays.asList(new PlayerState[4]);
-
-  /** A list of the actions taken by players in this phase, by player index. */
-  @JsonProperty("actions")
-  public List<Action> actions = Arrays.asList(new Action[4]);
+  @JsonProperty("turn")
+  public int turn;
 
   @JsonProperty("prev_phase")
   public GamePhaseType prev_phase;
 
   @JsonProperty("next_phase")
   public GamePhaseType next_phase;
+
+  /** A list of the actions taken by players in this phase, by player index. */
+  @JsonProperty("actions")
+  public List<Action> actions = Arrays.asList(new Action[4]);
+
+  /** A list of states of the 4 players. */
+  @JsonProperty("player_states")
+  public List<PlayerState> playerStates = Arrays.asList(new PlayerState[4]);
 }
